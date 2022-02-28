9ca128a04c4e8b17f29c90525be6e132b7402f6f
Alvaro Andres Pinzon Cortes <andresaaap@hotmail.com>
2022-02-28 10:12:57 -0500

    feat: code solution to send transactions to rinkeby

--- app.js
+++ app.js
@@ -1,27 +1,64 @@
-var Web3 = require('web3')
-var EthereumTransaction = require("ethereumjs-tx").Transaction
-//const EthereumTx = require('ethereumjs-tx').Transaction
-var url = 'HTTP://127.0.0.1:9545' // 8545 if using ganache-cli
-var web3 = new Web3(url)
-var sendingAddress = '0x5f0f037b77Ee98c9563Cb881fD91618E98Feefa2';
-var receivingAddress = '0xA0f0640474C8b2019E3E1F7C126096ac95738DDe';
-web3.eth.getBalance(sendingAddress).then(console.log) ;
-web3.eth.getBalance(receivingAddress).then(console.log);
-//web3.eth.getAccounts().then(accounts => console.log(accounts));
-/*##########################
-CREATE A TRANSACTION
-##########################*/
-// -- Step 4: Set up the transaction using the transaction variables as shown var rawTransaction = { nonce: 0, to: receivingAddress, gasPrice: 20000000, gasLimit: 30000, value: 1, data: "" }
-// -- Step 5: View the raw transaction rawTransaction
-// -- Step 6: Check the new account balances (they should be the same) web3.eth.getBalance(sendingAddress).then(console.log) web3.eth.getBalance(receivingAddress).then(console.log)
-// Note: They haven't changed because they need to be signed...
-var rawTransaction = { nonce: '0x0342', to: receivingAddress, gasPrice: '0x20000000', gasLimit: '0x30000', value: '0x01', data: "0x000" }
+// THIS IS THE LEGACY FORM TO SEND TRANSACTIONS
+// Loading dependencies
+const fs = require( 'fs' ).promises;
+const Web3 = require( 'web3' );
+const HDWalletProvider = require( '@truffle/hdwallet-provider' );
+const { mnemonicGenerate } = require( '@polkadot/util-crypto' );
+const Transaction = require('ethereumjs-tx').Transaction;
+const Common = require('ethereumjs-common').default;
 
-//Sign the transaction
-var privateKeySender ='ea1cc6f3eba8fdb5a551462cb3ba64e7f496df932c508e406ea66e45a476113e';
-var privateKeySenderHex = Buffer.from(privateKeySender, 'hex');
-var transaction = new EthereumTransaction(rawTransaction) ;
-transaction.sign(privateKeySenderHex);
-//Sign the transactions
-var serializedTransaction = transaction.serialize();
-web3.eth.sendSignedTransaction(serializedTransaction);
+async function main () {  
+    // Infura rinkeby's url
+    const infuraRinkeby = INFURA_HTTPS;
+
+    // Generating bip39 mnemonic
+    // const mnemonic = mnemonicGenerate();
+    // save the mnemonic in a JSON file in your project directory
+    // console.log(mnemonic);
+
+    // Loading previously generated mnemonic
+    const mnemonic = ( JSON.parse( await fs.readFile(
+                       "FILE_WITH_MNEMONIC.json" ,
+                       "utf8" 
+                      ) ) ).mnemonic;
+
+    // Generating provider
+    const provider = new HDWalletProvider( mnemonic , infuraRinkeby );
+    const web3 = new Web3( provider );
+
+    // Declaring rinkeby testnet
+    const chain = new Common( 'rinkeby' , 'istanbul' );
+
+    // Getting sending and receiving addresses
+    //YOU CAN CHANGE 0 TO SELECT OTHER ADDRESSES
+    const sendingAddress = ( await web3.eth.getAccounts() )[0]; 
+    const receivingAddress = "DESTINATION_ADDRESS";
+
+    // Getting the private key for the account
+    const preKey = ( provider.wallets )[ sendingAddress.toLowerCase() ]
+                   .privateKey.toString( 'hex' );
+    const privateKey = Buffer.from( preKey , 'hex' );
+
+    // Constructing the raw transaction
+    const rawTx = {
+        from        :   web3.utils.toHex( sendingAddress ),
+        to          :   web3.utils.toHex( receivingAddress ),
+        gasPrice    :   web3.utils.toHex( web3.utils.toWei( '1' , 'gwei' ) ),
+        gasLimit    :   web3.utils.toHex( 200000 ),
+        value       :   web3.utils.toHex( web3.utils.toWei( '0.25' , 'ether' ) ),
+        data        :   web3.utils.toHex( 'Hello World!' ),
+        nonce       :   web3.utils.toHex( await web3.eth.getTransactionCount( 
+                                          sendingAddress ,
+                                          'pending'
+                                         ) ),
+    };
+
+    // Creating a new transaction
+    const tx = new Transaction( rawTx , { common : chain } );
+
+    // Signing the transaction
+    tx.sign( privateKey );
+
+    // Sending transaction
+    await web3.eth.sendSignedTransaction( '0x' + tx.serialize().toString( 'hex' ) )
+    .on(
